55da57cc9fd85fff9b8417b1ba56ea1bcf30b184
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-09-18 18:39:37 -0300

    feat: Completed CRUD operations in controller

--- src/main/java/demo/com/controller/AnimeController.java
+++ src/main/java/demo/com/controller/AnimeController.java
@@ -5,11 +5,9 @@ import demo.com.service.AnimeService;
 import demo.com.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -33,4 +31,22 @@ public class AnimeController {
     public ResponseEntity<Anime> findById(@PathVariable Long id) {
         return ResponseEntity.ok(animeService.findById(id));
     }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(anime);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
--- src/main/java/demo/com/service/AnimeService.java
+++ src/main/java/demo/com/service/AnimeService.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
-    ;
+    private final List<Anime> animes;
+
+    {
+        animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+    }
+
 
     public List<Anime> findAll() {
         return animes;
@@ -21,5 +26,20 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found."));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(long id) {
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        animes.add(anime);
     }
 }
