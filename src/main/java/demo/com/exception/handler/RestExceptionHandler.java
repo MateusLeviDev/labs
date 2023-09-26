f3fb92ab6eb543953a5558a54c2e4a7fda8f5de5
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-09-25 23:13:48 -0300

    chore: implement new handlers. using ResponseEntityExceptionHandler. Overriding Spring handler

--- src/main/java/demo/com/exception/handler/RestExceptionHandler.java
+++ src/main/java/demo/com/exception/handler/RestExceptionHandler.java
@@ -1,37 +1,76 @@
 package demo.com.exception.handler;
 
-import demo.com.exception.BadRequestException;
-import demo.com.exception.ElementNotFoundException;
+import demo.com.exception.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@ControllerAdvice //os demais controllers devem utilizar as flags
-public class RestExceptionHandler {
+@ControllerAdvice
+@Log4j2
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<DefaultExceptionDetails> handlerBadRequestException(BadRequestException bre) {
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre) {
         return new ResponseEntity<>(
-                DefaultExceptionDetails.builder()
+                BadRequestExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
                         .status(HttpStatus.BAD_REQUEST.value())
-                        .title("Check the documentation")
+                        .title("Bad Request Exception, Check the Documentation")
                         .details(bre.getMessage())
                         .developerMessage(bre.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ElementNotFoundException.class)
-    public ResponseEntity<DefaultExceptionDetails> elementNotFoundException(ElementNotFoundException efe) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        String fields = fieldErrors.stream()
+                .map(FieldError::getField)
+                .collect(Collectors.joining(", "));
+
+        String fieldsMessage = fieldErrors.stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
         return new ResponseEntity<>(
-                DefaultExceptionDetails.builder()
+                ValidationExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
-                        .status(HttpStatus.NOT_FOUND.value())
-                        .title("Resource not found")
-                        .details(efe.getMessage())
-                        .developerMessage(efe.getClass().getName())
-                        .build(), HttpStatus.NOT_FOUND);
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Invalid Fields")
+                        .details("Check the field(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
     }
 }
