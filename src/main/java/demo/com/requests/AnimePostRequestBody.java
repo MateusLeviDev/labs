f623e116ee8bfdd6336e401c7d161d18647f881f
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-10-10 20:53:18 -0300

    `chore:` add validation name att

--- src/main/java/demo/com/requests/AnimePostRequestBody.java
+++ src/main/java/demo/com/requests/AnimePostRequestBody.java
@@ -7,6 +7,6 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePostRequestBody {
-    @NotEmpty(message = "Anime cannot be null")
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
