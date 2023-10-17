b7010c1f7919b7f6a5cd53b9398bac7d2d63d0a0
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-10-16 21:28:22 -0300

    `test:` UnitTest - start controller implementation

--- src/test/java/demo/com/controller/AnimeControllerTest.java
+++ src/test/java/demo/com/controller/AnimeControllerTest.java
@@ -1,8 +1,50 @@
 package demo.com.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import demo.com.domain.Anime;
+import demo.com.requests.AnimePostRequestBody;
+import demo.com.requests.AnimePutRequestBody;
+import demo.com.service.AnimeService;
+import demo.com.util.AnimeCreator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
 class AnimeControllerTest {
 
+    @InjectMocks
+    private AnimeController animeController;
+    @Mock
+    private AnimeService animeServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        PageImpl<Anime> animePage = new PageImpl<>(List.of(AnimeCreator.createValidAnime()));
+        BDDMockito.when(animeServiceMock.listAll(ArgumentMatchers.any()))
+                .thenReturn(animePage);
+
+        BDDMockito.when(animeServiceMock.listAllNonPageable())
+                .thenReturn(List.of(AnimeCreator.createValidAnime()));
+
+        BDDMockito.when(animeServiceMock.findByIdOrThrowBadRequestException(ArgumentMatchers.anyLong()))
+                .thenReturn(AnimeCreator.createValidAnime());
+
+        BDDMockito.when(animeServiceMock.findByName(ArgumentMatchers.anyString()))
+                .thenReturn(List.of(AnimeCreator.createValidAnime()));
+
+        BDDMockito.when(animeServiceMock.save(ArgumentMatchers.any(AnimePostRequestBody.class)))
+                .thenReturn(AnimeCreator.createValidAnime());
+
+        BDDMockito.doNothing().when(animeServiceMock).replace(ArgumentMatchers.any(AnimePutRequestBody.class));
+
+        BDDMockito.doNothing().when(animeServiceMock).delete(ArgumentMatchers.anyLong());
+    }
 
 }
