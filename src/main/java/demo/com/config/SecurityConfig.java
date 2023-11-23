836711f829b3a7af005e7589dca43be0807a4c5e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-11-23 20:32:11 -0300

    `raw:` Adding method-level security with PreAuthori

--- src/main/java/demo/com/config/SecurityConfig.java
+++ src/main/java/demo/com/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package demo.com.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.Customizer;import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,36 +10,41 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-    public SecurityConfig(InMemoryUserDetailsManager userDetailsManager) {
-    }
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity http, InMemoryUserDetailsManager userDetailsManager) throws Exception {
         http.csrf(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(Customizer.withDefaults())
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()); // Habilita o CSRF e define o token no cookie
 
         return http.build();
     }
-
+    //O token CSRF (XSRF-Token) é geralmente usado para proteger contra ataques CSRF
+    // (Cross-Site Request Forgery).
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
 
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-        UserDetails user = User.withUsername("diego")
-                .password(encoder.encode("maradona"))
+        UserDetails user = User.withUsername("Levi")
+                .password(encoder.encode("user_password"))
                 .roles("USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.withUsername("Mateus")
+                .password(encoder.encode("admin_password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
