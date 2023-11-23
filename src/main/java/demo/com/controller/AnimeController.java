bd906e95a6369009bbf362be51c970e3e5b807f8
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-11-23 20:32:34 -0300

    `raw:` Adding @PreAuthorize("hasRole('ADMIN')")

--- src/main/java/demo/com/controller/AnimeController.java
+++ src/main/java/demo/com/controller/AnimeController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -57,6 +58,7 @@ public class AnimeController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
         return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
